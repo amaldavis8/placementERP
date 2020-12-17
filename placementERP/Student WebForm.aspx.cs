18fd0c3e1d902652f4441a17e8f28c3dbf7bf531
Liya Varghese <liyakulangara@gmail.com>
2020-12-17 19:48:01 +0530

    fetchpersonal

--- placementERP/Student WebForm.aspx.cs
+++ placementERP/Student WebForm.aspx.cs
@@ -18,17 +18,25 @@ namespace placementERP
             {
                 string user = Convert.ToString(Session["New"]);
                 db = new Database();
-                string[] s = new string[5];
+                string[] s = new string[9];
+               
 
                 //details() defined inside Database.cs file
                 s = db.details(user);
+
 
                 cgpa.Text = s[0];
                 arrears.Text = s[1];
                 sslc.Text = s[2];
                 hse.Text = s[3];
                 studyGap.Text = s[4];
-
+                fullname_txt.Text = s[5];
+                dob_txt.Text = s[6];
+                pin_txt.Text = s[7];
+                email_txt.Text = s[8];
+                cnt_no_txt.Text = s[9];
+                DropDownList1.SelectedValue = s[10];
+                city_txt.Text = s[11];
                 double fcgpa = Convert.ToDouble(cgpa.Text);
                 double percent = (fcgpa * 10) - 3.75;
                 btechPer.Text = Convert.ToString(percent);
--- placementERP/Student WebForm.aspx.designer.cs
+++ placementERP/Student WebForm.aspx.designer.cs
@@ -22,40 +22,40 @@ namespace placementERP {
         protected global::System.Web.UI.WebControls.Button saveBtn;
         
         /// <summary>
-        /// TextBox1 control.
+        /// fullname_txt control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+        protected global::System.Web.UI.WebControls.TextBox fullname_txt;
         
         /// <summary>
-        /// TextBox2 control.
+        /// dob_txt control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox2;
+        protected global::System.Web.UI.WebControls.TextBox dob_txt;
         
         /// <summary>
-        /// TextBox3 control.
+        /// cnt_no_txt control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox3;
+        protected global::System.Web.UI.WebControls.TextBox cnt_no_txt;
         
         /// <summary>
-        /// TextBox4 control.
+        /// email_txt control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox4;
+        protected global::System.Web.UI.WebControls.TextBox email_txt;
         
         /// <summary>
         /// DropDownList1 control.
@@ -67,22 +67,22 @@ namespace placementERP {
         protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
         
         /// <summary>
-        /// TextBox6 control.
+        /// city_txt control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox6;
+        protected global::System.Web.UI.WebControls.TextBox city_txt;
         
         /// <summary>
-        /// TextBox7 control.
+        /// pin_txt control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox7;
+        protected global::System.Web.UI.WebControls.TextBox pin_txt;
         
         /// <summary>
         /// TextBox5 control.
