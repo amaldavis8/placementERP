4921e45474b1e50ca0ad52c43e7e2470bde79e37
amaldavis8 <40700557+amaldavis8@users.noreply.github.com>
2021-02-05 01:59:15 +0530

    PO Condition check for placement (Filtering Students)

--- placementERP/PO Web Form.aspx.designer.cs
+++ placementERP/PO Web Form.aspx.designer.cs
@@ -74,5 +74,23 @@ namespace placementERP {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox arrearHistory_txt;
+        
+        /// <summary>
+        /// submitBtn control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button submitBtn;
+        
+        /// <summary>
+        /// GridView1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridView1;
     }
 }
